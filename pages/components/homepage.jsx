ea81a43a71c3ded948ef1b23dd03a11e292d3b8c
Steven Sinha <stevensinha329@gmail.com>
2022-10-15 19:58:39 -0400

    add analyze sharing menu

--- pages/components/homepage.jsx
+++ pages/components/homepage.jsx
@@ -1,12 +1,15 @@
 import axios from 'axios';
 import { useState, useEffect} from 'react';
-import { Container, Navbar, NavDropdown, Button } from "react-bootstrap";
+import { Container, Navbar, Button, Modal, Form} from "react-bootstrap";
 import SnapshotCard from './snapshotCard';
 
 export default function HomePage(props) {
     const [snapshots, setSnapshots] = useState([])
     const [selecting, setSelecting] = useState(false)
     const [analyze, setAnalyze] = useState("")
+    const [path, setPath] = useState("")
+    const [drive, setDrive] = useState("")
+    const [show, setShow] = useState(false)
     const [selectedFiles, setSelectedFiles] = useState([])
 
     useEffect( () => {
@@ -21,11 +24,32 @@ export default function HomePage(props) {
     }, []);
 
     function toggleSelect(e, index) {
-        let new_selected = []
-        if (selectedFiles.indexOf(snapshots[index]) < 0 ) {
-            new_selected.push(snapshots[index])
+        let new_selected
+        if(analyze !== "Sharing Changes") {
+            new_selected = []
+            if (selectedFiles.indexOf(snapshots[index]) < 0 ) {
+                new_selected.push(snapshots[index])
+            }
+            setSelectedFiles(new_selected)
         }
-        setSelectedFiles(new_selected)
+        else {
+            new_selected = [...selectedFiles]
+            if (new_selected.includes(snapshots[index])) {
+                new_selected.splice(new_selected.indexOf(snapshots[index]), 1)
+                setSelectedFiles(new_selected)
+                return
+            }
+            else{
+                if(new_selected.length == 2) {
+                    new_selected.shift()
+                    new_selected.push(snapshots[index])
+                }
+                else{
+                    new_selected.push(snapshots[index])
+                }
+                setSelectedFiles(new_selected)
+            }
+        }
     }
 
     function handleViewClick(e) {
@@ -39,8 +63,24 @@ export default function HomePage(props) {
         if(analyze === "") {
             props.viewCallback(selectedFiles)
         }
+        else{
+            // switch to call analysis functions
+            console.log(`analyze type: ${analyze}`)
+            console.log(`analyze path: ${path}`)
+            console.log(`analyze drive: ${drive}`)
+            console.log(`snapshot: ${selectedFiles[0]}`)
+        }
     }
 
+    const handleClose = () => setShow(false)
+    const handleOpen = () => setShow(true)
+    function handleAnalyze(e) {
+        e.preventDefault();
+        handleClose()
+        setSelecting(true)
+        toggleSelect(null, 0)
+    }
+    
     let windowContents = "Take a snapshot to begin"
     if(snapshots.length > 0) {
         windowContents =
@@ -51,7 +91,7 @@ export default function HomePage(props) {
             </Container>
     }
 
-    let buttonEnabled = (selectedFiles.length === 1 && analyze !== "compare") || (selectedFiles.length === 2 && analyze === "compare")
+    let buttonEnabled = (selectedFiles.length === 1 && analyze !== "Sharing Changes") || (selectedFiles.length === 2 && analyze === "Sharing Changes")
     let confirmNavbar = <Navbar fixed="bottom" bg='light' className="justify-content-center">
                             <Button onClick={handleConfirmClick} size="lg" variant='success' disabled={!buttonEnabled}> Confirm Selected Files</Button>
                         </Navbar>
@@ -62,8 +102,55 @@ export default function HomePage(props) {
                 <Container>
                     <Navbar.Brand> Snapshots: </Navbar.Brand>
                     <Button onClick={handleViewClick}>View & Search Snapshot Files</Button> 
-                    <Button>Analyze Snapshot</Button>
+                    <Button onClick={handleOpen}>Analyze Snapshot</Button>
                 </Container>
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                    <Modal.Title>Analyze Snapshot</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                    <Form onSubmit={handleAnalyze}>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Select Analysis Type</Form.Label>
+                            <Form.Control
+                                as="select"
+                                value={analyze}
+                                onChange={e => {
+                                    setAnalyze(e.target.value);
+                                }}>
+                                <option>Redundant Sharing</option>
+                                <option>Deviant Sharing</option>
+                                <option>File-Folder Sharing Differences</option>
+                                <option>Sharing Changes</option>
+                            </Form.Control>
+                        </Form.Group>
+                       
+                        <Form.Group className="mb-3" controlId="formBasicPath">
+                            <Form.Label>Path (Optional)</Form.Label>
+                            <Form.Control type="path" placeholder="Enter path" onChange={e => setPath(e.target.value)}/>
+                            <Form.Text className="text-muted">
+                                    Only perform analysis on files matching a certain path
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicDrive">
+                            <Form.Label>Drive (Optional)</Form.Label>
+                            <Form.Control type="drive" placeholder="Drive" onChange={e => setDrive(e.target.value)}/>
+                            <Form.Text className="text-muted">
+                                 Only perform analysis on files within a certain drive
+                            </Form.Text>
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Select Snapshot
+                        </Button>
+                    </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    </Modal.Footer>
+                </Modal>
             </Navbar>
             {windowContents}
             {selecting?confirmNavbar:""}
