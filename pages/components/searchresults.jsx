5e807c34584350ebcc82fa4c2cada4364abac0f3
Steven Sinha <stevensinha329@gmail.com>
2022-10-14 20:03:54 -0400

    move search logic to function in searchresults component

--- pages/components/searchresults.jsx
+++ pages/components/searchresults.jsx
@@ -26,13 +26,7 @@ export default function SearchResults(props) {
 
     console.log(`searching ${query}`)
     let search = new SearchQuery(query)
-    if(search.valid === false){
-      setQuery("invalid")
-      return
-    }
-    else {
-      filtered = FilterFiles(files, search)
-    }
+    filtered = FilterFiles(files, search)
   }
 
   return (
@@ -56,4 +50,60 @@ export default function SearchResults(props) {
       </Container>
     </>
   );
+}
+
+function FilterFiles(files, query) {
+  let query_fields = queryString.split(" ")
+  for (let field of query_fields) {
+      if (field.indexOf(':') === -1){
+          return {files: files, valid: false}
+      }
+
+      let type = field.substring(0, field.indexOf(':'))
+      let value = field.substring(field.indexOf(':')+1)
+      console.log(`query type: ${type} \nfor value ${value}`)
+
+      switch(type) {
+          case "drive": this.drive = value
+          break
+
+          case "owner": this.owner = value
+          break
+
+          case "creator": this.creator = value
+          break
+
+          case "from": this.from = value
+          break
+
+          case "to": this.to = value
+          break
+
+          case "readable": this.readable = value
+          break
+
+          case "writable": this.writable = value
+          break
+
+          case "shareable": this.shareable = value
+          break
+
+          case "name": this.name = value
+          break
+
+          case "inFolder": this.inFolder = value
+          break
+
+          case "folder": this.folder = value
+          break
+
+          case "path": this.path = value
+          break
+
+          case "sharing": this.sharing = value
+          break
+
+          default: {this.valid = false; return}          
+      }
+  }
 }
