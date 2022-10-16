40001991b012e2f0fab09e633b8bf888268fb51b
Steven Sinha <stevensinha329@gmail.com>
2022-10-16 19:16:16 -0400

    stylistic changes to code of results modal

--- pages/components/AnalysisResultsModal.jsx
+++ pages/components/AnalysisResultsModal.jsx
@@ -53,7 +53,7 @@ function printError(index) {
 }
 
 function createDeviantShareListCard(result, index) {
-    let str = `File \"${result.file}\" has deviant permissions compared to folder \"${result.parent}\"` 
+    let str = `File [${result.file}] has deviant permissions compared to folder [${result.parent}]` 
 
     return (
         <ListGroup.Item action href={`#link${index+1}`} key={index}>
@@ -66,33 +66,33 @@ function createDeviantShareExplanation(result, index) {
     let content = 
         <Container>
             <Row>
-                The majority of files in folder "{result.parent}" have the following permissions:
+                The majority of files in folder [{result.parent}] have the following permissions:
             </Row>
-                {result.standard_perms.map((permission_string) => {
+                {result.standard_perms.map((permission_string, index) => {
                     return (
-                    <Row>
+                    <Row key={index}>
                         <Col sm='1'></Col>
                         <Col>{permission_string}</Col>
                     </Row> )
                 })}
             <Row><p></p></Row>
             <Row>
-                While file "{result.file}" has the following permissions:
+                While file [{result.file}] has the following permissions:
             </Row>
-                {result.file_perms.map((permission_string) => {
+                {result.file_perms.map((permission_string, index) => {
                         return (
-                        <Row>
+                        <Row key={index}>
                             <Col sm='1'></Col>
                             <Col>{permission_string}</Col>
                         </Row> )
                     })}
             <Row><p></p></Row>
             <Row>
-                Permissions that "{result.file}" has that the majority of files in "{result.parent}" do not:
+                Permissions that [{result.file}] has that the majority of files in [{result.parent}] do not:
             </Row>
-                {result.extra.map((permission_string) => {
+                {result.extra.map((permission_string, index) => {
                         return (
-                        <Row>
+                        <Row key={index}>
                             <Col sm='1'></Col>
                             <Col>{permission_string}</Col>
                         </Row> )
@@ -100,11 +100,11 @@ function createDeviantShareExplanation(result, index) {
 
             <Row><p></p></Row>
             <Row>
-                Permissions that the majority of files in "{result.parent}" have that "{result.file}" does not:
+                Permissions that the majority of files in [{result.parent}] have that [{result.file}] does not:
             </Row>
-                {result.missing.map((permission_string) => {
+                {result.missing.map((permission_string, index) => {
                         return (
-                        <Row>
+                        <Row key={index}>
                             <Col sm='1'></Col>
                             <Col>{permission_string}</Col>
                         </Row> )
@@ -119,7 +119,7 @@ function createDeviantShareExplanation(result, index) {
 }
 
 function createFileFolderListCard(result, index) {
-    let str = `File \"${result.file}\" has different permissions from parent folder \"${result.parent}\"` 
+    let str = `File [${result.file}] has different permissions from parent folder [${result.parent}]` 
 
     return (
         <ListGroup.Item action href={`#link${index+1}`} key={index}>
@@ -132,33 +132,33 @@ function createFileFolderExplanation(result, index) {
     let content = 
         <Container>
             <Row>
-                Parent folder "{result.parent}" has the following permissions:
+                Parent folder [{result.parent}] has the following permissions:
             </Row>
-                {result.parent_perms.map((permission_string) => {
+                {result.parent_perms.map((permission_string, index) => {
                     return (
-                    <Row>
+                    <Row key={index}>
                         <Col sm='1'></Col>
                         <Col>{permission_string}</Col>
                     </Row> )
                 })}
             <Row><p></p></Row>
             <Row>
-                While file "{result.file}" has the following permissions:
+                While file [{result.file}] has the following permissions:
             </Row>
-                {result.file_perms.map((permission_string) => {
+                {result.file_perms.map((permission_string, index) => {
                         return (
-                        <Row>
+                        <Row key={index}>
                             <Col sm='1'></Col>
                             <Col>{permission_string}</Col>
                         </Row> )
                     })}
             <Row><p></p></Row>
             <Row>
-                Permissions that "{result.file}" has that "{result.parent}" does not:
+                Permissions that [{result.file}] has that [{result.parent}] does not:
             </Row>
-                {result.extra.map((permission_string) => {
+                {result.extra.map((permission_string, index) => {
                         return (
-                        <Row>
+                        <Row key={index}>
                             <Col sm='1'></Col>
                             <Col>{permission_string}</Col>
                         </Row> )
@@ -166,11 +166,11 @@ function createFileFolderExplanation(result, index) {
 
             <Row><p></p></Row>
             <Row>
-                Permissions that "{result.parent}" has that "{result.file}" does not:
+                Permissions that [{result.parent}] has that [{result.file}] does not:
             </Row>
-                {result.missing.map((permission_string) => {
+                {result.missing.map((permission_string, index) => {
                         return (
-                        <Row>
+                        <Row key={index}>
                             <Col sm='1'></Col>
                             <Col>{permission_string}</Col>
                         </Row> )
@@ -187,10 +187,10 @@ function createFileFolderExplanation(result, index) {
 function createSharingChangesListCard(result, index) {
     let str = ""
     if (result.type === "new") {
-        str = `File \"${result.file}\" is a new file in Snapshot ${result.second_snapshot_id}` 
+        str = `File [${result.file}] is a new file in Snapshot ${result.second_snapshot_id}` 
     }
     else {
-        str = `File \"${result.file}\" has permissions that changed between Snapshots` 
+        str = `File [${result.file}] has permissions that changed between Snapshots` 
     }
 
     return (
@@ -206,11 +206,11 @@ function createSharingChangesExplanation(result, index) {
         content = 
         <Container>
             <Row>
-                File "{result.file}" is a new file with the following permissions:
+                File [{result.file}] is a new file with the following permissions:
             </Row>
-                {result.permissions.map((permission_string) => {
+                {result.permissions.map((permission_string, index) => {
                     return (
-                    <Row>
+                    <Row key={index}>
                         <Col sm='1'></Col>
                         <Col>{permission_string}</Col>
                     </Row> )
@@ -221,33 +221,33 @@ function createSharingChangesExplanation(result, index) {
         content = 
         <Container>
             <Row>
-                File "{result.file}" had the following permissions in the first Snapshot:
+                File [{result.file}] had the following permissions in the first Snapshot:
             </Row>
-                {result.first_snapshot_perms.map((permission_string) => {
+                {result.first_snapshot_perms.map((permission_string, index) => {
                     return (
-                    <Row>
+                    <Row key={index}>
                         <Col sm='1'></Col>
                         <Col>{permission_string}</Col>
                     </Row> )
                 })}
             <Row><p></p></Row>
             <Row>
-                While file "{result.file}" has the following permissions in the more recent Snapshot:
+                While file [{result.file}] has the following permissions in the more recent Snapshot:
             </Row>
-                {result.second_snapshot_perms.map((permission_string) => {
+                {result.second_snapshot_perms.map((permission_string, index) => {
                         return (
-                        <Row>
+                        <Row key={index}>
                             <Col sm='1'></Col>
                             <Col>{permission_string}</Col>
                         </Row> )
                     })}
             <Row><p></p></Row>
             <Row>
-                Permissions that "{result.file}" has in the new Snapshot that it did not in the earlier:
+                Permissions that [{result.file}] has in the new Snapshot that it did not in the earlier:
             </Row>
-                {result.new_perms.map((permission_string) => {
+                {result.new_perms.map((permission_string, index) => {
                         return (
-                        <Row>
+                        <Row key={index}>
                             <Col sm='1'></Col>
                             <Col>{permission_string}</Col>
                         </Row> )
@@ -255,11 +255,11 @@ function createSharingChangesExplanation(result, index) {
 
             <Row><p></p></Row>
             <Row>
-                Permissions that "{result.file}" had in the earlier Snapshot that it does not in the more recent:
+                Permissions that [{result.file}] had in the earlier Snapshot that it does not in the more recent:
             </Row>
-                {result.deleted_perms.map((permission_string) => {
+                {result.deleted_perms.map((permission_string, index) => {
                         return (
-                        <Row>
+                        <Row key={index}>
                             <Col sm='1'></Col>
                             <Col>{permission_string}</Col>
                         </Row> )
