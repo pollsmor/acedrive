55f953ca88c0aa9cb144c441af314dc0bd85629f
Tron <tarun.sharma@stonybrook.edu>
2022-10-30 19:23:59 +0530

    Refactored the Code for searching to an api

--- pages/api/searchSnapShot.js
+++ pages/api/searchSnapShot.js
@@ -1,13 +1,89 @@
+import lodash from 'lodash'
 import { getToken } from 'next-auth/jwt';
 import Snapshot from '../../lib/models/Snapshot';
+
+const queryOperator = {
+  "drive":"driveName",
+  "owner":"owners",
+  "creator":"user",//TODO:Check Where We can get this key
+  "from":"sharingUser",
+  "to":"user",
+  "readable":"user",
+  "writable":"user",
+  "sharable":"user",
+  "name":"regexp",
+  "inFolder":"regexp",
+  "folder":"regexp",
+  "path":"path",
+  "sharing":"none",
+  "sharing":"anyone",
+  "sharing":"individual",
+  "sharing":"domain",
+  "foldersonly":"true"
+}
 
 export default async function getSnapshot(req, res) {
   const token = await getToken({ req });
   if (token) {
-    let snapshot_id = req.query.id;
-    let snapshot = await Snapshot.findById(snapshot_id).lean();
-    res.json(snapshot);
+    const snapshotId = req.query.id;
+    const searchFor = req.query.query;
+    const snapshot = await Snapshot.findById(snapshotId);
+    const filteredResults = await searchSnapShot (snapshot.files , searchFor);
+    res.json(filteredResults);
   } else {
     res.end('Invalid session.');
   }
+}
+
+const searchSnapShot = (files,query) => {
+  const splitQuery = query.split(":");
+    let searchForKeywords = query;
+    let fieldTofetch = "name";
+
+    if(splitQuery.length > 0){
+      if(Object.keys(queryOperator).includes(splitQuery[0])){
+        fieldTofetch = queryOperator[splitQuery[0]];
+        searchForKeywords = splitQuery[1];
+      }
+    }
+
+    if (query === '' || searchForKeywords === ''){
+      return (files);
+    }
+
+    let searchedFiles = [];
+
+    if(splitQuery[0] === "owner" || splitQuery[0] === "from" ){
+      searchedFiles = files.filter((file)=>{
+        const fileDetails = file[fieldTofetch];
+        let matchedDetails = [];
+        if(Array.isArray(fileDetails)){
+          matchedDetails = lodash.filter(fileDetails,(detail)=>{
+            console.log(detail)
+            if(detail.displayName.toLowerCase().search(searchForKeywords.toLowerCase()) !== -1){
+              return detail;
+            }
+          })
+          if(matchedDetails.length > 0){
+            return file;
+          }
+        }else{
+          if(fileDetails){
+            console.log(fileDetails)
+            if(fileDetails?.displayName.toLowerCase().search(searchForKeywords.toLowerCase()) !== -1){
+              return file;
+            }
+          }
+        }
+      })
+    }else{
+      searchedFiles = files.filter((file)=>{
+        const fileName = file[fieldTofetch].toLowerCase();
+        if(fileName.search(searchForKeywords.toLowerCase()) !== -1){
+          return file;
+        }
+      })
+    }
+    
+    return searchedFiles;
 }
--- pages/snapshot/[snapshotID].jsx
+++ pages/snapshot/[snapshotID].jsx
@@ -1,32 +1,14 @@
 import axios from 'axios';
-import { useState, useEffect } from 'react';
-import { Container, ListGroup, Form, FormControl, Button } from 'react-bootstrap';
+
 import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
+
 import Banner from '../../components/Banner';
-import AnalysisForm from '../../components/AnalysisForm';
 import FileCard from '../../components/FileCard';
 import FolderCard from '../../components/FolderCard';
-import lodash from 'lodash'
+import AnalysisForm from '../../components/AnalysisForm';
 
-const queryOperator = {
-  "drive":"driveName",
-  "owner":"owners",
-  "creator":"user",//TODO:Check Where We can get this key
-  "from":"sharingUser",
-  "to":"user",
-  "readable":"user",
-  "writable":"user",
-  "sharable":"user",
-  "name":"regexp",
-  "inFolder":"regexp",
-  "folder":"regexp",
-  "path":"path",
-  "sharing":"none",
-  "sharing":"anyone",
-  "sharing":"individual",
-  "sharing":"domain",
-  "foldersonly":"true"
-}
+import { Container, ListGroup, Form, FormControl } from 'react-bootstrap';
 
 export default function Snapshot() {
   const router = useRouter();
@@ -54,59 +36,21 @@ export default function Snapshot() {
     if (snapshotID) fetchSnapshot();
   }, [snapshotID]);
 
-  const searchSnapShot = (e) => {
+  const fetchSnapshotSearch = async(snapshotId,query) =>{
+      try {
+        const results = await axios.get('/api/searchSnapShot', { 
+          params: { id: snapshotId ,query: query}
+        });
+        return (results.data)
+      } catch (err) {
+        alert('Nothing Matched The Query');
+      }
+
+  }
+  const searchSnapShot = async (e) => {
     e.preventDefault();
     
-
-    const splitQuery = query.split(":");
-    let searchForKeywords = query;
-    let fieldTofetch = "name";
-
-    if(splitQuery.length > 0){
-      if(Object.keys(queryOperator).includes(splitQuery[0])){
-        fieldTofetch = queryOperator[splitQuery[0]];
-        searchForKeywords = splitQuery[1];
-      }
-    }
-
-    if (query === '' || searchForKeywords === ''){
-      setFilteredFiles(files);
-      return;
-    }
-
-    let searchedFiles = [];
-
-    if(splitQuery[0] === "owner" || splitQuery[0] === "from" ){
-      searchedFiles = files.filter((file)=>{
-        const fileDetails = file[fieldTofetch];
-        let matchedDetails = [];
-        if(Array.isArray(fileDetails)){
-          matchedDetails = lodash.filter(fileDetails,(detail)=>{
-            console.log(detail)
-            if(detail.displayName.toLowerCase().search(searchForKeywords.toLowerCase()) !== -1){
-              return detail;
-            }
-          })
-          if(matchedDetails.length > 0){
-            return file;
-          }
-        }else{
-          if(fileDetails){
-            console.log(fileDetails)
-            if(fileDetails?.displayName.toLowerCase().search(searchForKeywords.toLowerCase()) !== -1){
-              return file;
-            }
-          }
-        }
-      })
-    }else{
-      searchedFiles = files.filter((file)=>{
-        const fileName = file[fieldTofetch].toLowerCase();
-        if(fileName.search(searchForKeywords.toLowerCase()) !== -1){
-          return file;
-        }
-      })
-    }
+    const searchedFiles = await fetchSnapshotSearch(snapshotID,query);
     setFilteredFiles(searchedFiles);
   }
 
