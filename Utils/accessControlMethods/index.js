eaee50b3bc890c6e3890c1ee00ca0af7c095dd01
Tron <tarun.sharma@stonybrook.edu>
2022-12-20 21:51:16 +0530

    Code Refactor

--- Utils/accessControlMethods/index.js
+++ Utils/accessControlMethods/index.js
@@ -1,83 +1,70 @@
 import lodash from "lodash";
 
-export function allowedAccessControlCheck(files, searchTerm, role) {
-    const updatedSearchTerm = searchTerm.replace(/}|{/g, "");
-    const findForUsers = updatedSearchTerm.split(",");
-    const searchedFiles = files.filter((file) => {
-        const fileDetails = file["permissions"];
-        let matchedDetails = [];
-        if (Array.isArray(fileDetails)) {
-            matchedDetails = lodash.filter(fileDetails, (detail) => {
-                if (detail.type !== "anyone" && detail.type !== "domain") {
+/*  
+  Checking for Access Controls for the specified query
+  Checking for the domain or email provided in the query on the file permissions
+  Returning files that are following the Access Conditions provided in the Query
+*/
+export function accessControlCheck(files, searchTerm, role, type) {
+    try {
+        const updatedSearchTerm = searchTerm.replace(/}|{/g, "");
+        const findForUsers = updatedSearchTerm.split(",");
+        const validFiles = files.filter((file) => {
+            const fileDetails = file["permissions"];
+            let matchedDetails = [];
+            if (Array.isArray(fileDetails)) {
+                matchedDetails = lodash.filter(fileDetails, (detail) => {
+                    if (detail.type !== "anyone" && detail.type !== "domain") {
+                        if (
+                            findForUsers.includes(detail.email.toLowerCase()) &&
+                            detail.role.toLowerCase() === role.toLowerCase()
+                        ) {
+                            return detail;
+                        }
+                    }
                     if (
-                        findForUsers.includes(detail.email.toLowerCase()) &&
+                        detail.type === "domain" &&
                         detail.role.toLowerCase() === role.toLowerCase()
                     ) {
-                        return detail;
+                        if (
+                            findForUsers.includes(detail.domain.toLowerCase())
+                        ) {
+                            return detail;
+                        }
                     }
+                });
+                if (matchedDetails.length > 0) {
+                    return file;
                 }
-                if (
-                    detail.type === "domain" &&
-                    detail.role.toLowerCase() === role.toLowerCase()
-                ) {
-                    if (findForUsers.includes(detail.domain.toLowerCase())) {
-                        return detail;
-                    }
-                }
-            });
-            if (matchedDetails.length > 0) {
+            }
+        });
+
+        //Filtering the violated files
+        const violatedFiles = files.filter((file) => {
+            if (!validFiles.includes(file)) {
                 return file;
             }
-        }
-    });
-    const deniedFiles = files.filter((file) => {
-        if (!searchedFiles.includes(file)) {
-            return file;
-        }
-    });
-    return { files: searchedFiles, violated: deniedFiles };
+        });
+
+        //returning files depending on the condition if it allowed/denied
+        return {
+            files: type ? violatedFiles : validFiles,
+            violated: type ? validFiles : violatedFiles,
+        };
+    } catch (error) {
+        throw error;
+    }
 }
 
-export function deniedAccessControlCheck(files, searchTerm, role) {
-    const updatedSearchTerm = searchTerm.replace(/}|{/g, "");
-    const findForUsers = updatedSearchTerm.split(",");
-    const searchedFiles = files.filter((file) => {
-        const fileDetails = file["permissions"];
-        let matchedDetails = [];
-        if (Array.isArray(fileDetails)) {
-            matchedDetails = lodash.filter(fileDetails, (detail) => {
-                if (detail.type !== "anyone" && detail.type !== "domain") {
-                    if (
-                        findForUsers.includes(detail.email.toLowerCase()) &&
-                        detail.role.toLowerCase() === role.toLowerCase()
-                    ) {
-                        return detail;
-                    }
-                }
-                if (
-                    detail.type === "domain" &&
-                    detail.role.toLowerCase() === role.toLowerCase()
-                ) {
-                    if (findForUsers.includes(detail.domain.toLowerCase())) {
-                        return detail;
-                    }
-                }
-            });
-            if (matchedDetails.length > 0) {
-                return file;
-            }
-        }
-    });
-    const deniedFiles = files.filter((file) => {
-        if (!searchedFiles.includes(file)) {
-            return file;
-        }
-    });
-    return { files: deniedFiles, violated: searchedFiles };
-}
-
+/*  
+  Checking for Access Controls for the specified query
+  Checking for if the file has a Group Access Control Specified in the permissions
+  Returning files that are following the Access Conditions provided in the Query
+*/
 export function groupAccessControlCheck(files, searchTerm) {
     const updatedSearchTerm = searchTerm.replace(/}|{/g, "");
+
+    //converting the string value of true or false we are getting from the Query
     const boolean = updatedSearchTerm.toLowerCase() === "true";
     const searchedFiles = files.filter((file) => {
         const fileDetails = file["permissions"];
--- algorithms/AccessControl.js
+++ algorithms/AccessControl.js
@@ -1,47 +1,60 @@
 import {
-  allowedAccessControlCheck,
-  deniedAccessControlCheck,
-  groupAccessControlCheck
+    accessControlCheck,
+    groupAccessControlCheck,
 } from "../Utils/accessControlMethods";
 
+// Using An Operator List object to what method to be used and other factors for the query
 const operatorsList = {
-  AR:{
-    method: "allowedAccessControlCheck",
-    role: "reader"
-  },
-  AW:{
-    method: "allowedAccessControlCheck",
-    role: "writer"
-  },
-  DR:{
-    method: "deniedAccessControlCheck",
-    role: "reader"
-  },
-  DW:{
-    method: "deniedAccessControlCheck",
-    role: "writer"
-  },
-  GRP:{
-    method: "groupAccessControlCheck",
-  }
+    AR: {
+        method: "accessControlCheck",
+        role: "reader",
+        denined: false,
+    },
+    AW: {
+        method: "accessControlCheck",
+        role: "writer",
+        denined: false,
+    },
+    DR: {
+        method: "accessControlCheck",
+        role: "reader",
+        denined: true,
+    },
+    DW: {
+        method: "accessControlCheck",
+        role: "writer",
+        denined: true,
+    },
+    GRP: {
+        method: "groupAccessControlCheck",
+    },
 };
 
-
+// Access Control Check Methods
 const searchMethods = {
-  allowedAccessControlCheck: allowedAccessControlCheck,
-  deniedAccessControlCheck : deniedAccessControlCheck,
-  groupAccessControlCheck:groupAccessControlCheck
+    accessControlCheck: accessControlCheck,
+    groupAccessControlCheck: groupAccessControlCheck,
 };
 
-export default async function getAccessControls(files,query) {
-  try{
-      const splitQuery = query.split(":");
-      const accessCheckFor = operatorsList[splitQuery[0].toUpperCase()];
-      const searchUsing = searchMethods[accessCheckFor.method];
-      const results = await searchUsing(files,splitQuery[1],accessCheckFor?.role);
+export default async function getAccessControls(files, query) {
+    try {
+        const updatedQuery = query.replaceAll(" ", "");
+        const splitQuery = updatedQuery.split(":");
+        const accessCheckFor = operatorsList[splitQuery[0].toUpperCase()];
+        const searchUsing = searchMethods[accessCheckFor.method];
+        const results = await searchUsing(
+            files,
+            splitQuery[1],
+            accessCheckFor?.role,
+            accessCheckFor?.denined
+        );
 
-      return {status:"200", files: results.files, filesInViolation: results.violated };
-  }catch(error){
-    return {status:"400",msg:"Please Provide A Valid Query"}
-  }
+        return {
+            status: "200",
+            files: results.files,
+            filesInViolation: results.violated,
+        };
+    } catch (error) {
+        return { status: "400", msg: "Please Provide A Valid Query" };
+    }
 }
